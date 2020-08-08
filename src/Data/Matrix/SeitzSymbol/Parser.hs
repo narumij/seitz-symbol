bd69ec3e0ece970bba888773c8f4f5c805abfc2a
narumij <narumij@me.com>
2020-08-09 08:57:26 +0900

    reference and license

--- src/Data/Matrix/SeitzSymbol/Parser.hs
+++ src/Data/Matrix/SeitzSymbol/Parser.hs
@@ -9,6 +9,8 @@ Seitz Symbol parser and etc.
 
 [References]
 
+Michael Glazer,a Mois I. Aroyo and Andre ́: Authier Acta Cryst. (2014). A70
+
 ネスポロ マッシモ:日本結晶学会誌 59，210-222(2017).
 https://www.jstage.jst.go.jp/article/jcrsj/59/5/59_210/_pdf
 
